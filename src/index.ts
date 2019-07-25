a6b3e86dbacd08906712920a99994454db2aba2a
hardik shah <hardik.shah@move.com>
2019-07-25 12:42:05 -0700

    wired up..

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+const app = express();
+const port = 8080;
+
+// Configure Express to use EJS
+app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "ejs" );
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    // render the index template
+    res.render( "index" );
+} );
+
+app.listen(port, () => {
+    console.log(`server is running at http://localhost:${port}`);
+});
